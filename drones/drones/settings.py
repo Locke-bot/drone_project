bd01e9a52f61921f53ea8a95ee27d0f66bd8d6fc
Oluwatobi Sholanke <tobisholanke@gmail.com>
2022-01-15 06:52:48 +0100

    Install MYSQL connector to support MYSQL

--- drones/drones/settings.py
+++ drones/drones/settings.py
@@ -76,9 +76,17 @@ WSGI_APPLICATION = 'drones.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'musala_assignment',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
