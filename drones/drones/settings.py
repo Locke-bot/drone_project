a920a47acd2498fe8a403c69db92c3c4f62f07c5
Oluwatobi Sholanke <tobisholanke@gmail.com>
2022-01-15 19:34:00 +0100

    change DB back to Local SQLite

--- drones/drones/settings.py
+++ drones/drones/settings.py
@@ -78,18 +78,18 @@ WSGI_APPLICATION = 'drones.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
-        'ENGINE': 'mysql.connector.django',
-        'NAME': 'musala_assignment',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+    # 'default': {
+    #     'ENGINE': 'mysql.connector.django',
+    #     'NAME': 'musala_assignment',
+    #     'USER': 'root',
+    #     'PASSWORD': '',
+    #     'HOST': 'localhost',
+    #     'PORT': '3306',
+    # }
 }
 
 
